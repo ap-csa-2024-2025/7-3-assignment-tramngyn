6795b350b5ebe337c345431e3395999d48bf59cb
tramngyn <bvnguyen115@student.hbuhsd.edu>
2025-02-21 11:21:08 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,12 +5,22 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Feel free to test code here
+    ArrayList<String> letters = new ArrayList<String>();
+    letters.add("a");
+    letters.add("b");
+    letters.add("c");
+    letters.add("d");
+    letters.add("e");
+    letters.add("f");
+    shiftLeft(letters);
   }
 
   public static void shiftLeft(ArrayList<String> arr)
   {
-    
+    System.out.println("ArrayList original: " + arr);
+    String front = arr.remove(0);
+    arr.add(front);
+    System.out.println("ArrayList result: " + arr);
   }
 
   public static printStatistics(final ArrayList<Integer> arr)
